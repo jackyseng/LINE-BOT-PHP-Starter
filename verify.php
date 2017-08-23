46611b796617cf1358c7855bf5d709298e834df0
jackyseng <jackyseng_99@hotmail.com>
2017-08-24 00:42:13 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,4 @@
 <?php
-//6ug9J/v9heYKJoEz371t/yBwrePY9aep4eVbvHX+oRVw0fhsPd6adQcBfgiqPsNslZ2QrnbPrxgX0MtLo9hCajqRJtUYkxxHhTnpRFi0fV15XXhvhQrJmXU5JPhbBX78T1KfKoXh1Ku4lmoPiuo5hwdB04t89/1O/w1cDnyilFU=
-$access_token = '6ug9J/v9heYKJoEz371t/yBwrePY9aep4eVbvHX+oRVw0fhsPd6adQcBfgiqPsNslZ2QrnbPrxgX0MtLo9hCajqRJtUYkxxHhTnpRFi0fV15XXhvhQrJmXU5JPhbBX78T1KfKoXh1Ku4lmoPiuo5hwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
